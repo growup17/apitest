ed31023b2740325cf3dfecf748be8e636e18b212
liubinqiang <liubinqiang1990@qq.com>
2022-06-29 16:47:01 +0800

    init

---
+++ src/test/java/run/codeis/apitest/ApitestApplicationTests.java
@@ -0,0 +1,13 @@
+package run.codeis.apitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
